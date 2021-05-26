fe6db7c1e0e062fae63ec52d3e26bfc249b40d9a
Sergey_Artemiev <sergArtmiev0024@yandex.ru>
2021-05-26 18:56:26 +0500

    Project initialization

---
+++ src/main/java/ru/host/hostTestTask/HostTestTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.host.hostTestTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/host/hostTestTask/HostTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.host.hostTestTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
