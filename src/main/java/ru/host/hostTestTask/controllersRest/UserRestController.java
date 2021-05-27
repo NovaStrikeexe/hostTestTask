c3dbe04034186a0bdc6622169075fd6838f701cd
Sergey_Artemiev <sergArtmiev0024@yandex.ru>
2021-05-27 19:15:37 +0500

    20210527minor2

--- src/main/java/ru/host/hostTestTask/controllersRest/UserRestController.java
+++ src/main/java/ru/host/hostTestTask/controllersRest/UserRestController.java
@@ -1,23 +1,22 @@
-package ru.host.hostTestTask.controllers;
+package ru.host.hostTestTask.controllersRest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import ru.host.hostTestTask.dto.Patient;
+import ru.host.hostTestTask.dto.PatientDto;
 
 import java.time.LocalDate;
 
 @RestController
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserController {
     @GetMapping("/user")
+
     public String getUserInfo(){
         final String lname,fname,mName,snils,enp;
         final LocalDate bDate;
-        Patient patient = new Patient();
-        return null; /*Заглушка*/
-        /*переделать после*/
+        PatientDto patientDto = new PatientDto();
+        return "user";
     }
 
 }
