8b266aecd0de24599c40ebaa147f639d60dd7027
phantomnova0024@gmail.com <45460546+NovaStrikeexe@users.noreply.github.com>
2021-06-02 14:53:59 +0500

    Started work with soap

---
+++ src/main/java/ru/host/hostTestTask/serviceSoap/Config.java
@@ -0,0 +1,26 @@
+package ru.host.hostTestTask.serviceSoap;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+@Configuration
+public class Config {
+    @Bean
+    public Jaxb2Marshaller marshaller() {
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        // this is the package name specified in the <generatePackage> specified in
+        // pom.xml
+        marshaller.setContextPath("com.example.howtodoinjava.schemas.school");
+        return marshaller;
+    }
+
+    @Bean
+    public UserServiceSoap userServiceSoap(Jaxb2Marshaller marshaller) {
+        UserServiceSoap user = new UserServiceSoap();
+        user.setDefaultUri("http://localhost:8080/user");
+        user.setMarshaller(marshaller);
+        user.setUnmarshaller(marshaller);
+        return user;
+    }
+}
--- src/main/java/ru/host/hostTestTask/serviceSoap/UserServiceSoap.java
+++ src/main/java/ru/host/hostTestTask/serviceSoap/UserServiceSoap.java
@@ -3,10 +3,14 @@ package ru.host.hostTestTask.serviceSoap;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 
 @Service
 @Scope(value = "session")
 @Component(value = "userServiceSoap")
-public class UserServiceSoap {
+public class UserServiceSoap extends WebServiceGatewaySupport {
+    public Object callWebService(String url, Object request){
+        return getWebServiceTemplate().marshalSendAndReceive(url, request);
+    }
 
 }
