7103ab5b2c8d714539a5b4a8a36e2a1ec7e37877
phantomnova0024@gmail.com <45460546+NovaStrikeexe@users.noreply.github.com>
2021-06-10 11:51:27 +0500

    Work with SoapControler06102021

--- src/main/java/ru/host/hostTestTask/conrtollersSoap/UserController.java
+++ src/main/java/ru/host/hostTestTask/conrtollersSoap/UserController.java
@@ -1,5 +1,8 @@
 package ru.host.hostTestTask.conrtollersSoap;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,14 +11,16 @@ import org.springframework.ws.soap.client.core.SoapActionCallback;
 import ru.hostco.reguser.types.ObjectFactory;
 import ru.hostco.reguser.types.GetUserRequestType;
 import ru.hostco.reguser.types.GetUserResponseType;
-
-@Controller
+@Controller("UserController")
+@RequestMapping("/user")
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class UserController extends WebServiceGatewaySupport {
+    //@Value("${src.main.resources.application.yaml}")
     private String soapToken = "D468E929-A94E-4F16-A7D2-DB414EC53071";
     private final String soapUrl = "http://www.hostco.ru/reguser";
     private final String getUser = "/getUser";
 
-    @RequestMapping(value = "user", method = RequestMethod.GET)
+
     public GetUserResponseType getUser(String snils) {
         ObjectFactory objectFactory = new ObjectFactory();
         GetUserRequestType getUserRequestType = objectFactory.createGetUserRequestType();
